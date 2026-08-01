ff65e57a8150a0df754348ca3481b8db22633a00
Maruf109 <marufxyz1524@gmail.com>
2026-08-01 17:56:47 +0600

    PAlindrom test by list

--- Project_list.py
+++ Project_list.py
@@ -1,7 +1,19 @@
-movies = []
+# movies = []
 
-movies.append(input("Enter your 1st movie: "))
-movies.append(input("Enter your 2nd movie: "))
-movies.append(input("Enter your 3rd movie: "))
+# movies.append(input("Enter your 1st movie: "))
+# movies.append(input("Enter your 2nd movie: "))
+# movies.append(input("Enter your 3rd movie: "))
 
-print(movies)
+# print(movies)
+
+pal = [1,2,3,2,1]
+
+pal2 = pal.copy()
+pal2.reverse()
+
+if(pal2 == pal):
+    print("Its palindrom")
+else:
+    print("Not Palindrom")
+
+
