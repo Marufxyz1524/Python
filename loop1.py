afde7a42fdcacd4c694dec128ecaf7f87067c44a
Maruf109 <marufxyz1524@gmail.com>
2026-08-30 12:22:58 +0600

    Loop Basic Part 1

---
+++ loop1.py
@@ -0,0 +1,22 @@
+# nums = (1,2,3,3,3,4,5,6,7,8,9,2,2,40)
+
+# x = 4
+# i = 0
+
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("Found at index =",i)
+#         break
+#     else:
+#         print("finding....")
+#         i += 1
+
+i = 1
+while i <=10:
+    if(i % 2 != 0):
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+        
