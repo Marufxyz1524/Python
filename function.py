4293e278b49a4e0e5469f9bf6d81f72af1691f9e
Maruf109 <marufxyz1524@gmail.com>
2026-09-03 23:09:48 +0600

    Function and recursion

--- function.py
+++ function.py
@@ -31,10 +31,24 @@
 #     print(fact)
 # cal_fact(6)        
 
-def show(n):
+# def show(n):
+#     if(n==0):
+#      return
+#     print(n)        
+#     show(n-1)
+#     print("END")
+# show(3)        
+
+# def fact(n):
+#     if(n==0 or n== 1):
+#         return 1
+#     else:
+#         return n * fact(n-1)
+# print(fact(4))
+
+def calc_sum(n):
     if(n==0):
-     return
-    print(n)        
-    show(n-1)
-    print("END")
-show(3)        
+        return 0
+    else:
+        return calc_sum(n-1) + n
+print(calc_sum(5))    
