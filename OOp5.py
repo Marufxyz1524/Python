77511b57304aeee2e806962c4395e4402af06369
MarufxB <marufxyz1524@gmail.com>
2026-09-22 23:03:02 +0600

    Practise drlf

--- OOp5.py
+++ OOp5.py
@@ -12,7 +12,9 @@ class DOG:
              print(f"{self.name} rolled over")
 
 my_dog = DOG("Willie",6)
-print(f"My dog name is {my_dog.name} ")
-print(f"He {my_dog.age} years old")             
+# print(f"My dog name is {my_dog.name} ")
+# print(f"He {my_dog.age} years old")             
+# my_dog.sit()
+# my_dog.roll_over()
                    
        
