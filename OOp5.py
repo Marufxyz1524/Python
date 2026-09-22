753b4a7c15705ffc1bb12b9cbeaed0f98803ce71
MarufxB <marufxyz1524@gmail.com>
2026-09-22 19:15:12 +0600

    Self Practise

---
+++ OOp5.py
@@ -0,0 +1,18 @@
+##creating dog class
+class DOG:
+ 
+      def __init__(self,name,age):
+             self.name = name
+             self.age  = age
+
+      def sit(self):
+             print(f"{self.name} in now sitting")
+
+      def roll_over(self):
+             print(f"{self.name} rolled over")
+
+my_dog = DOG("Willie",6)
+print(f"My dog name is {my_dog.name} ")
+print(f"He {my_dog.age} years old")             
+                   
+       
