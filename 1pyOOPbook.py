f4f42ba16c1d5c8f24acff9d1b9268ee23d0f9fe
MarufxB <marufxyz1524@gmail.com>
2026-09-24 17:51:22 +0600

    Default Values

--- 1pyOOPbook.py
+++ 1pyOOPbook.py
@@ -3,13 +3,20 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.miles = 0
 
     def get_descriptive(self):
 
         long_name = f"{self.year} {self.model} {self.year}"
         return long_name.title()  
 
+    def read_odometar(self):
+        print(f"This car has {self.miles} mile on a road")
+
+
 my_new_car = Car("Ferrari","m6",2025)
 print(my_new_car.get_descriptive())
+my_new_car.read_odometar()
+
 
     
